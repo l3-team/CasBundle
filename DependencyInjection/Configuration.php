0df9948b875ea9f403dc56de5278631bbcd3bb82
Hetru Mathieu <mathieu.hetru@univ-lille.fr>
2019-07-11 11:22:45 +0200

    correction for deprecated config rootNode for SF4.2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,10 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('l3_cas');
-
-        $rootNode
+        $treeBuilder = new TreeBuilder('l3_cas');
+        $treeBuilder->getRootNode()
             ->children()
             ->scalarNode('host')->defaultValue(300)->end()
             ->scalarNode('path')->defaultValue('')->end()
